6e28f029766b2e82026920eaad0deae0bfe3843a
Hemant katurde <hemantkaturde123@gmail.com>
2022-10-13 23:13:02 +0530

    added new changes

--- application/views/frontend/category.php
+++ application/views/frontend/category.php
@@ -4,6 +4,18 @@
 			<span><em></em></span>
 			<h2><?php echo get_phrase('categories'); ?></h2>
 		</div>
+		<div class ="row">
+		<div class="col-lg-6">
+      </div>
+		<div class="col-lg-6 mb-4">
+		<div class="widget search_blog">
+				<!-- <label><?php echo get_phrase('title'); ?>, <?php echo get_phrase('description'); ?>, <?php echo get_phrase('category'); ?></label> -->
+				<div class="form-group">
+					<input type="text" name="search" value="<?php if($searching_value != ''){ echo $searching_value; } ?>" id="searching_key" class="form-control" placeholder="<?php echo get_phrase('search'); ?>..">
+					<span><input type="submit" id="blog_searching_btn" onclick="blog_search()" value="<?php echo get_phrase('search'); ?>" style="cursor: pointer;"></span>
+				</div>
+		</div>
+        </div>
 		<div class="row justify-content-center">
 			<?php
 			foreach ($categories as $key => $category): ?>
@@ -27,3 +39,31 @@
 		</div>
 	</div>
 </div>
+
+
+<script>
+	function blog_search(){
+		var searching_value = $('#searching_key').val();
+		if(searching_value != ''){
+			location.replace("<?php echo site_url('home/category?search='); ?>"+searching_value);
+		}else{
+			location.replace("<?php echo site_url('home/category'); ?>");
+		}
+	}
+
+	// Get the input field
+var input = document.getElementById("searching_key");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("blog_searching_btn").click();
+
+}
+});
+
+	</script>
